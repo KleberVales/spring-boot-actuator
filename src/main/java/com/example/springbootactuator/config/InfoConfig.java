86f74ce0e19bcfa067ea49161d52a5529b5d08d1
Kleber Vales <klebervales.dev@gmail.com>
2025-09-02 13:24:57 -0300

    Class infoConfig

--- src/main/java/com/example/springbootactuator/config/InfoConfig.java
+++ src/main/java/com/example/springbootactuator/config/InfoConfig.java
@@ -1,4 +1,21 @@
-package com.example.springbootactuator.config;
+package com.kleber.actuator.config;
 
-public class InfoConfig {
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class InfoConfig implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, Object> details = new HashMap<>();
+        details.put("author", "Kleber");
+        details.put("project", "Spring Boot Actuator Demo");
+        details.put("version", "1.0.0");
+        builder.withDetail("app-info", details);
+    }
 }
